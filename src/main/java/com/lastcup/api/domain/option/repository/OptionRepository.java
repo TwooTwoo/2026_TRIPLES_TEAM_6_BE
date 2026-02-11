6167811c2239f9353241b8357e03e517d7672e4d
TwooTwoo <mbnman12@gmail.com>
2026-02-11 22:27:36 +0900

    feat: OptionRepository에 영양 정보 포함 조회 쿼리 추가

--- src/main/java/com/lastcup/api/domain/option/repository/OptionRepository.java
+++ src/main/java/com/lastcup/api/domain/option/repository/OptionRepository.java
@@ -5,6 +5,8 @@ import com.lastcup.api.domain.option.domain.OptionCategory;
 import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
 
@@ -13,4 +15,7 @@ public interface OptionRepository extends JpaRepository<Option, Long> {
     List<Option> findByBrandIdAndCategory(Long brandId, OptionCategory category);
 
     long countByIdIn(Collection<Long> ids);
+
+    @Query("SELECT o FROM Option o LEFT JOIN FETCH o.nutrition WHERE o.id IN :ids")
+    List<Option> findAllWithNutritionByIdIn(@Param("ids") Collection<Long> ids);
 }
